1e3675406174c62f8f02b2fd53f973bdaaae5a4f
gamesIQ007 <bdagames@ya.ru>
2022-10-18 22:08:07 +0300

    16.2. Базовый скрипт космических объектов Подготовлены заготовки скриптов Entity и Destructible.

---
+++ Assets/Space Shooter/Scripts/Destructible.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    /// <summary>
+    /// Уничтожаемый объект. То, что может иметь хитпоинты.
+    /// </summary>
+    public class Destructible : Entity
+    {
+        #region Properties
+
+        /// <summary>
+        /// Объект игнорирует повреждения.
+        /// </summary>
+        [SerializeField]
+        private bool m_Indestructible;
+        public bool IsIndestructible => m_Indestructible;
+
+        /// <summary>
+        /// Стартовое количество хитпоинтов.
+        /// </summary>
+        [SerializeField]
+        private int m_HitPoints;
+
+        /// <summary>
+        /// Текущее количество хитпоинтов.
+        /// </summary>
+        private int m_CurrentHitPoints;
+        public int HitPoints => m_CurrentHitPoints;
+
+        #endregion
+
+        #region Unity Events
+
+        protected virtual void Start()
+        {
+            m_CurrentHitPoints = m_HitPoints;
+        }
+
+        #endregion
+
+        #region Public API
+
+        /// <summary>
+        /// Применение урона к объекту.
+        /// </summary>
+        /// <param name="damage">Наносимый объекту урон.</param>
+        public void ApplyDamage(int damage)
+        {
+            if (m_Indestructible) return;
+
+            m_CurrentHitPoints -= damage;
+            if (m_CurrentHitPoints <= 0)
+            {
+                OnDeath();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Переопределяемое событие уничтожения объекта, когда хитпоинты меньше 0.
+        /// </summary>
+        protected virtual void OnDeath()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
