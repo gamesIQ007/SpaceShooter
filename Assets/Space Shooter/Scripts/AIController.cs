2fe203f81fe00afc56cbd0b3900fe1cf0a57e0ad
gamesIQ007 <bdagames@ya.ru>
2022-11-16 20:25:22 +0300

    19.3. State Machine ИИ: Перемещение к целевой точке

--- Assets/Space Shooter/Scripts/AIController.cs
+++ Assets/Space Shooter/Scripts/AIController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 namespace SpaceShooter
 {
+    [RequireComponent(typeof(SpaceShip))]
+
     /// <summary>
     /// Искусственный интеллект
     /// </summary>
@@ -68,27 +70,119 @@ namespace SpaceShooter
         /// </summary>
         private Destructible m_SelectedTarget;
 
-        private Timer testTimer;
-
         #region Unity Events
 
         private void Start()
         {
-            testTimer = new Timer(3);
+            m_SpaceShip = GetComponent<SpaceShip>();
+
+            InitTimers();
         }
 
         private void Update()
         {
-            testTimer.RemoveTime(Time.deltaTime);
+            UpdateTimers();
 
-            if (testTimer.IsFinished)
-            {
-                Debug.Log("таймер!");
-
-                testTimer.Start(3);
-            }
+            UpdateAI();
         }
 
         #endregion
+
+        #region Timers
+
+        /// <summary>
+        /// Инициализация таймеров
+        /// </summary>
+        private void InitTimers()
+        {
+
+        }
+
+        /// <summary>
+        /// Обновление таймеров
+        /// </summary>
+        private void UpdateTimers()
+        {
+
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Обновление ИИ
+        /// </summary>
+        private void UpdateAI()
+        {
+            if (m_AIBehaviour == AIBehaviour.Patrol)
+            {
+                UpdateBehaviourPatrol();
+            }
+        }
+
+        /// <summary>
+        /// Обновить поведение при патрулировании
+        /// </summary>
+        private void UpdateBehaviourPatrol()
+        {
+            ActionFindNewMovePosition();
+            ActionControlShip();
+            ActionFindNewAttackTarget();
+            ActionFire();
+        }
+
+        /// <summary>
+        /// Найти новую цель перемещения
+        /// </summary>
+        private void ActionFindNewMovePosition()
+        {
+
+        }
+
+        /// <summary>
+        /// Управление перемещением
+        /// </summary>
+        private void ActionControlShip()
+        {
+            m_SpaceShip.ThrustControl = m_NavigationLinear;
+            m_SpaceShip.TorqueControl = ComputeAliginTorqueNormalized(m_MovePosition, m_SpaceShip.transform) * m_NavigationAngular;
+        }
+
+        /// <summary>
+        /// Максимальный угол поворота
+        /// </summary>
+        private const float MAX_ANGLE = 45.0f;
+
+        /// <summary>
+        /// Расчёт множителя для поворота между целью и кораблём
+        /// </summary>
+        /// <param name="targetPosition">Цель</param>
+        /// <param name="ship">Корабль</param>
+        /// <returns>Множитель поворота</returns>
+        private static float ComputeAliginTorqueNormalized(Vector3 targetPosition, Transform ship)
+        {
+            Vector2 localTargetPosition = ship.InverseTransformPoint(targetPosition);
+
+            float angle = Vector3.SignedAngle(localTargetPosition, Vector3.up, Vector3.forward);
+
+            angle = Mathf.Clamp(angle, -MAX_ANGLE, MAX_ANGLE) / MAX_ANGLE;
+
+            return -angle;
+        }
+
+        /// <summary>
+        /// Поиск новой цели атаки
+        /// </summary>
+        private void ActionFindNewAttackTarget()
+        {
+
+        }
+
+        /// <summary>
+        /// Атака
+        /// </summary>
+        private void ActionFire()
+        {
+
+        }
     }
 }
